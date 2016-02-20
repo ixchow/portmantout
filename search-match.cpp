a4bf4fb9817fabee30eb08250f71df9b277cd60e
Jim McCann <ix@tchow.com>
2016-02-20 02:04:24 -0500

    we may be getting somewhere, though it's unexpected

--- search-match.cpp
+++ search-match.cpp
@@ -1,6 +1,10 @@
+#include <limits>
+#include <algorithm>
+#include <map>
+
 #include "graph.hpp"
 #include "stopwatch.hpp"
-#include "blossom5/PerfectMatching.h"
+
 
 int main(int argc, char **argv) {
 
@@ -21,6 +25,31 @@ int main(int argc, char **argv) {
 	}
 	std::cout << "Have " << maximal.size() << " maximal nodes." << std::endl;
 
+	uint32_t start = -1U;
+	for (auto const &m : maximal) {
+		std::string prefix;
+		uint32_t at = m;
+		while (graph.parent[at] < graph.nodes) {
+			int32_t parent = graph.parent[at];
+			bool found = false;
+			for (uint32_t a = graph.adj_start[parent]; a != graph.adj_start[parent+1]; ++a) {
+				if (graph.adj[a] == at) {
+					assert(!found);
+					found = true;
+					prefix += graph.adj_char[a];
+				}
+			}
+			assert(found);
+			at = parent;
+		}
+		std::reverse(prefix.begin(), prefix.end());
+		if (prefix.substr(0, 11) == "portmanteau") {
+			start = &m - &maximal[0];
+			std::cout << prefix << std::endl; //DEBUG
+		}
+	}
+	assert(start != -1U);
+
 	std::unique_ptr< uint8_t[] > distances(new uint8_t[maximal.size() * maximal.size()]);
 
 	
@@ -34,29 +63,90 @@ int main(int argc, char **argv) {
 
 	//------------------------------------------
 
-	int64_t match_nodes = maximal.size();
-	if (match_nodes % 2 == 1) {
-		match_nodes += 1;
-	}
+	//goal: greedy (randomized) unification from lowest to highest cost.
 
-	PerfectMatching pm(match_nodes, match_nodes * (match_nodes - 1) / 2);
+	std::map< uint32_t, uint32_t > merges;
+
+	std::vector< std::pair< uint32_t, uint32_t > > particles;
 
 	for (uint32_t i = 0; i < maximal.size(); ++i) {
-		if (match_nodes > int64_t(maximal.size())) {
-			pm.AddEdge(i, match_nodes - 1, graph.depth[maximal[i]]); //"don't pair" edge costs length
-		}
-		for (uint32_t j = 0; j < i; ++j) {
-			assert(i * maximal.size() + j < maximal.size() * maximal.size());
-			assert(j * maximal.size() + i < maximal.size() * maximal.size());
-			//uint8_t cost = std::min(distances[i * maximal.size() + j], distances[j * maximal.size() + i]);
-			//pm.AddEdge(i, j, cost);
+		particles.emplace_back(i, i);
+	}
+
+	{
+		uint32_t total_length = 0;
+		//unify in cost order:
+		while (particles.size() > 1) {
+			std::cout << "At " << particles.size() << " particles." << std::endl;
+
+			std::vector< std::pair< uint32_t, uint32_t > > next_particles;
+			std::vector< bool > used(particles.size(), false);
+			while (1) {
+				std::vector< std::pair< uint32_t, uint32_t > > best;
+				int32_t best_cost = std::numeric_limits< int32_t >::max();
+				for (auto const &p1 : particles) {
+					if (used[&p1 - &particles[0]]) continue;
+					for (auto const &p2 : particles) {
+						if (&p2 == &p1) continue;
+						if (used[&p2 - &particles[0]]) continue;
+						if (p2.first == start) continue; //can't merge with start symbol, no matter how much one might wish to
+	
+						//p1 then p2 incurs:
+						int32_t cost = distances[p1.second * maximal.size() + p2.first];
+						cost -= int32_t(graph.depth[maximal[p2.first]]); //basically, cost is -overlap
+
+						if (cost < best_cost) {
+							best_cost = cost;
+							best.clear();
+						}
+						if (cost == best_cost) {
+							best.emplace_back(std::make_pair(&p1 - &particles[0], &p2 - &particles[0]));
+						}
+					}
+				}
+				if (best.empty()) break;
+				std::cout << "   " << best.size() << " have cost " << best_cost << std::endl;
+				for (auto b : best) {
+					if (used[b.first] || used[b.second]) continue;
+					used[b.first] = true;
+					used[b.second] = true;
+					merges.insert(std::make_pair(particles[b.first].second, particles[b.second].first));
+					next_particles.emplace_back(particles[b.first].first, particles[b.second].second);
+					total_length += distances[particles[b.first].second * maximal.size() + particles[b.second].first];
+				}
+				std::cout << "Total length so far: " << total_length << std::endl;
+			}
+			uint32_t extra = 0;
+			for (uint32_t p = 0; p < particles.size(); ++p) {
+				if (!used[p]) {
+					next_particles.emplace_back(particles[p]);
+					++extra;
+				}
+			}
+			std::cout << "Had " << extra << " leftover." << std::endl;
+			particles = std::move(next_particles);
 		}
 	}
-	stopwatch("build problem");
 
-	pm.Solve();
+	stopwatch("merge");
 
-	stopwatch("solve problem");
+	//------------------------------------------
+
+	std::vector< uint32_t > path;
+	{
+		uint32_t len = 0;
+		uint32_t at = start;
+		while (1) {
+			path.emplace_back(maximal[at]);
+			auto f = merges.find(at);
+			if (f == merges.end()) break;
+			len += distances[at * maximal.size() + f->second];
+			at = f->second;
+		}
+		std::cout << "Path of " << path.size() << " steps (extracted from " << merges.size() << " merges) -- len " << len << " (not counting first word)" << std::endl;
+		std::ofstream out("path.dump");
+		out.write(reinterpret_cast< const char * >(&path[0]), 4 * path.size());
+	}
 
 
 }
